ada09d750ee35a1a199f1e6ee9793b47c107ca3b
Serial Incubator <274148479+darian1504@users.noreply.github.com>
2026-05-05 10:11:34 +1000

    Copy sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'training-log-v17';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192px.png', './icon-512px.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  // Network-first: always try network, fall back to cache only if offline
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return r;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
