b1c48cbc333fe3846c3f262aeb1d2a484d60aca6
Serial Incubator <274148479+SerialIncubator@users.noreply.github.com>
2026-05-14 12:19:30 +1000

    fix: relative start_url/scope in manifest + bump SW cache v2

---
+++ www/sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'mytrainingmate-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192px.png', './icon-512px.png', './icon-1024.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(err => console.warn('SW cache failed:', err)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return r;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
